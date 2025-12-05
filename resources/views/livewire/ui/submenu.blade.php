fca062c2211334b3cc62e2cb00a877a5c893201d
webNekit <mochalov-hunter@mail.ru>
2025-12-05 23:24:38 +0300

    update scroll dropdown

--- resources/views/livewire/ui/submenu.blade.php
+++ resources/views/livewire/ui/submenu.blade.php
@@ -21,6 +21,120 @@
                     </ul>
                 </div>
             </li>
+            <li class="navbar__item dropdown-menu">
+                <a href="#!" class="dropdown-menu__trigger navbar__link">
+                    {{ $group['title'] }}
+                </a>
+
+                <div class="dropdown-menu__content" aria-hidden="true">
+                    <ul class="dropdown-menu__list">
+
+                        @foreach($group['items'] as $item)
+                            <li class="dropdown-menu__item">
+                                <a href="{{ $item['url'] }}" class="dropdown-menu__link">
+                                    {{ $item['name'] }}
+                                </a>
+                            </li>
+                        @endforeach
+
+                    </ul>
+                </div>
+            </li>
+            <li class="navbar__item dropdown-menu">
+                <a href="#!" class="dropdown-menu__trigger navbar__link">
+                    {{ $group['title'] }}
+                </a>
+
+                <div class="dropdown-menu__content" aria-hidden="true">
+                    <ul class="dropdown-menu__list">
+
+                        @foreach($group['items'] as $item)
+                            <li class="dropdown-menu__item">
+                                <a href="{{ $item['url'] }}" class="dropdown-menu__link">
+                                    {{ $item['name'] }}
+                                </a>
+                            </li>
+                        @endforeach
+
+                    </ul>
+                </div>
+            </li>
+            <li class="navbar__item dropdown-menu">
+                <a href="#!" class="dropdown-menu__trigger navbar__link">
+                    {{ $group['title'] }}
+                </a>
+
+                <div class="dropdown-menu__content" aria-hidden="true">
+                    <ul class="dropdown-menu__list">
+
+                        @foreach($group['items'] as $item)
+                            <li class="dropdown-menu__item">
+                                <a href="{{ $item['url'] }}" class="dropdown-menu__link">
+                                    {{ $item['name'] }}
+                                </a>
+                            </li>
+                        @endforeach
+
+                    </ul>
+                </div>
+            </li>
+            <li class="navbar__item dropdown-menu">
+                <a href="#!" class="dropdown-menu__trigger navbar__link">
+                    {{ $group['title'] }}
+                </a>
+
+                <div class="dropdown-menu__content" aria-hidden="true">
+                    <ul class="dropdown-menu__list">
+
+                        @foreach($group['items'] as $item)
+                            <li class="dropdown-menu__item">
+                                <a href="{{ $item['url'] }}" class="dropdown-menu__link">
+                                    {{ $item['name'] }}
+                                </a>
+                            </li>
+                        @endforeach
+
+                    </ul>
+                </div>
+            </li>
+            <li class="navbar__item dropdown-menu">
+                <a href="#!" class="dropdown-menu__trigger navbar__link">
+                    {{ $group['title'] }}
+                </a>
+
+                <div class="dropdown-menu__content" aria-hidden="true">
+                    <ul class="dropdown-menu__list">
+
+                        @foreach($group['items'] as $item)
+                            <li class="dropdown-menu__item">
+                                <a href="{{ $item['url'] }}" class="dropdown-menu__link">
+                                    {{ $item['name'] }}
+                                </a>
+                            </li>
+                        @endforeach
+
+                    </ul>
+                </div>
+            </li>
+            <li class="navbar__item dropdown-menu">
+                <a href="#!" class="dropdown-menu__trigger navbar__link">
+                    {{ $group['title'] }}
+                </a>
+
+                <div class="dropdown-menu__content" aria-hidden="true">
+                    <ul class="dropdown-menu__list">
+
+                        @foreach($group['items'] as $item)
+                            <li class="dropdown-menu__item">
+                                <a href="{{ $item['url'] }}" class="dropdown-menu__link">
+                                    {{ $item['name'] }}
+                                </a>
+                            </li>
+                        @endforeach
+
+                    </ul>
+                </div>
+            </li>
         @endforeach
 
     </menu>
