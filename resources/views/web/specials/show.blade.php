818574b84c62699e15b489ba1112579a14de3b0c
webNekit <mochalov-hunter@mail.ru>
2025-12-07 21:24:29 +0300

    update

--- resources/views/web/specials/show.blade.php
+++ resources/views/web/specials/show.blade.php
@@ -7,46 +7,74 @@
                         <div class="single-education__alt-code">{{ $special->code }}</div>
                         <h1 class="single-education__alt-name xl">{{ $special->title }}</h1>
                     </div>
+
                     <div class="single-education__program-learning program-learning">
                         <div class="program-learning__wrapper">
+
                             <div class="program-learning__head">
-                                <div class="program-learning__head-title"><span style="text-transform: capitalize;">{{ $special->form }}</span> форма обучения</div>
+                                <div class="program-learning__head-title">
+                                    <span style="text-transform: capitalize;">
+                                        {{ $special->form }}
+                                    </span>
+                                    форма обучения
+                                </div>
                             </div>
+
                             <div class="program-learning__body">
                                 <ul class="program-learning__list">
+
                                     <li class="program-learning__item">
                                         <div class="program-learning__key">&nbsp;</div>
                                         <div class="program-learning__value">
-                                            @if($special->cost == 0)
-                                                Бюджет
-                                            @else
-                                                Внебюджет
-                                            @endif
+                                            {{ $special->cost == 0 ? 'Бюджет' : 'Внебюджет' }}
                                         </div>
                                     </li>
+
                                     <li class="program-learning__item">
                                         <div class="program-learning__key">Квалификация</div>
-                                        <div class="program-learning__value">{{ $special->qualification }}</div>
+                                        <div class="program-learning__value">
+                                            {{ $special->qualification }}
+                                        </div>
                                     </li>
-                                    @if($special->level_middle)
+
+                                    {{-- На базе 9 классов (СКРЫВАЕМ ЦЕЛИКОМ, если категория = 2) --}}
+                                    @if($special->level_middle && $special->special_category_id != 2)
                                         <li class="program-learning__item">
-                                            <div class="program-learning__key">На базе 9 классов</div>
-                                            <div class="program-learning__value">{{ $special->level_middle }}</div>
+                                            <div class="program-learning__key">
+                                                На базе 9 классов
+                                            </div>
+                                            <div class="program-learning__value">
+                                                {{ $special->level_middle }}
+                                            </div>
                                         </li>
                                     @endif
+
+                                    {{-- На базе 11 классов (СКРЫВАЕМ ТОЛЬКО ТЕКСТ, если категория = 2) --}}
                                     @if($special->level_max)
                                         <li class="program-learning__item">
-                                            <div class="program-learning__key">На базе 11 классов</div>
-                                            <div class="program-learning__value">{{ $special->level_max }}</div>
+                                            <div class="program-learning__key">
+                                                @if($special->special_category_id != 2)
+                                                    На базе 11 классов
+                                                @else
+                                                    &nbsp;
+                                                @endif
+                                            </div>
+                                            <div class="program-learning__value">
+                                                {{ $special->level_max }}
+                                            </div>
                                         </li>
                                     @endif
+
                                 </ul>
                             </div>
                         </div>
                     </div>
+
                 </div>
             </div>
+
             <br>
+
             <div class="single-education__body">
                 <div class="single-education__container container">
                     <div class="single-education__context">
@@ -54,6 +82,7 @@
                     </div>
                 </div>
             </div>
+
         </section>
     </div>
 </x-app>
